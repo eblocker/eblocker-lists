cd5d8ad6e202e8f7f325ee3ca35a4bcd2967f8bb
eBlocker Open Source <dev@eblocker.org>
2020-05-18 15:54:42 +0200

    Remove unnecessary imports

--- src/main/java/org/eblocker/lists/parentalcontrol/ParentalControlFilterListCreator.java
+++ src/main/java/org/eblocker/lists/parentalcontrol/ParentalControlFilterListCreator.java
@@ -45,10 +45,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
